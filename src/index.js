14c0f7fb08066f8d521d486f9ed9b4bfe40a2efa
imane benamar <imanebenamar22@gmail.com>
2022-02-23 22:04:38 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Modal } from "./lib/modal"
+
+ReactDOM.render(
+    <Modal modalmessage='hello world!'color='#022334'/>,
+    document.getElementById("root"));
---
+++ src/lib/modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './modal.css';
+/** Display modal
+ * @param  {string} modalmessage
+ * @param  {string} color
+ * @param  {boolean} modalOpen
+ * @param  {function} modalClose
+ * @param  {string} buttonContent
+ */
+export const Modal = ({ modalmessage, color, modalClose, buttonContent }) => {
+    return (
+        <div className="modal">
+            <div onClick={modalClose} className="modalOverlay">   </div>
+            <div className="modalContent">
+                <p>{modalmessage}</p>
+                <button onClick={modalClose} style={{ background: `${color}` }} className="modalClose">
+                    {buttonContent}
+                </button>
+            </div>
+        </div>
+    )
+}
+
+
